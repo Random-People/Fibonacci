0b1a9ff8f74cad20b8895de9b8034e497f0bf32b
Safyre <lyons.safyre@gmail.com>
2019-09-24 00:56:15 -0600

    Create Fib.java

---
+++ Fib.java
@@ -0,0 +1,22 @@
+package start;
+
+public class Fib {
+	public static void main(String[] args) {
+		p(0);
+		int a = 1;
+		p(a);	
+		a = 1;
+		p(a);
+		int b = a + a;
+		p(b);
+		while(a < 300) {
+			a = a + b;
+			p(a);
+			b = a + b;
+			p(b);
+		}
+	}
+	public static void p(int outp) {
+		System.out.println(outp);
+	}
+}
