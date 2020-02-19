f48de08697d1afa4948e567528a1951592843d89
Kai <lyons.safyre@gmail.com>
2020-02-18 19:03:52 -0700

    Create fib.pl

---
+++ fib.pl
@@ -0,0 +1,12 @@
+$a = 1;
+$b = $a + $a;
+print "0\n";
+print "1\n";
+print "$a\n";
+print "$b\n";
+while ($a < 1000000){
+    $a = $a + $b;
+    $b = $a + $b;
+    print "$a\n";
+    print "$b\n"
+}
