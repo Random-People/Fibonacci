a7b25cfffae4e9f2b4fcb2655f295ada1d992c54
Kai Lyons <62919224+KaiLikesLinux@users.noreply.github.com>
2021-01-22 09:20:45 -0700

    Remove comment
    Original comments:
          -- remove the leading space (reserved for the sign); Natural guarantees
          -- that the number can never be negative, so we can safely take the
          -- substring without checking for the first character

--- fib.adb
+++ fib.adb
@@ -4,9 +4,6 @@ procedure Fib is
    procedure Print(Item : Natural) is
       S : constant String := Natural'Image(Item);
    begin
-      -- remove the leading space (reserved for the sign); Natural guarantees
-      -- that the number can never be negative, so we can safely take the
-      -- substring whithout checking for the first character
       Ada.Text_IO.Put_Line(S(S'First + 1 .. S'Last));
    end Print;
 
