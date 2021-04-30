45274a759b1eb6842ba188104b17a1b458a81a6c
Guillaume DEREX <gderex8@orange.fr>
2021-04-30 15:41:01 +0200

    Forgot to print the 0

--- fib.zig
+++ fib.zig
@@ -2,9 +2,11 @@ const std = @import("std");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-
+    
+    try stdout.print("0\n1\n", .{});
+    
     var a: u32 = 1;
-    try stdout.print("{}\n{}\n", .{ a, a });
+    try stdout.print("{}\n{}\n", .{a});
 
     var b: u32 = 2 * a;
     try stdout.print("{}\n", .{b});
