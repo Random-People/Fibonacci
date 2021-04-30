2486652b602ebf9f6f641b90e8d958578b8cc380
guillaumederex <gderex8@orange.fr>
2021-04-30 13:32:41 +0200

    Zig example didn't work

--- fib.zig
+++ fib.zig
@@ -1,21 +1,18 @@
-const warn = std.debug.warn;
 const std = @import("std");
-const os = std.os;
-const assert = std.debug.assert;
 
-pub fn main() void {
-    const zero: i32 = 0;
-    warn("{}\n", zero);
-    const one: i32 = 1;
-    warn("{}\n", one);
-    var a: i32 = 1;
-    warn("{}\n", a);
-    var b: i32 = a + a;
-    warn("{}\n", b);
-    while (a < 1000000){
-        a = a + b;
-        warn("{}\n", a);
-        b = a + b;
-        warn("{}\n", b);
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+
+    var a: u32 = 1;
+    try stdout.print("{}\n{}\n", .{ a, a });
+
+    var b: u32 = 2 * a;
+    try stdout.print("{}\n", .{b});
+
+    while (a < 1000000) {
+        a += b;
+        try stdout.print("{}\n", .{a});
+        b += a;
+        try stdout.print("{}\n", .{b});
     }
 }
